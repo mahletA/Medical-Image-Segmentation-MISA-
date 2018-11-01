813c56bd8c19ef8ccfb25d78025e110a07b3c7d5
Mahlet <mahleta.b735@gmail.com>
2018-11-01 01:57:52 +0100

    Background and Iteration fixed

--- Lab2/Code/Initialize.m
+++ Lab2/Code/Initialize.m
@@ -1,13 +1,23 @@
 %% INITIALIZATION %%
 
-function [alpha, Mu, Sigma] = Initialize (X,k)
+function [alpha, Mu, Sigma] = Initialize (X,K)
 
 % X is an N x f matrix
 N = size(X,1);
-alpha = 1/k* ones(1,k);
+f = size(X,2);
+alpha = 1/K* ones(1,K);
 
-[~,Mu] = kmeans(X,k);
+[idx,Mu] = kmeans(X,K);
+
 Sigma = cov(X);
+% Sigma = zeros(K*f,f);
+% for k = 1:K
+%      if k ==1
+%         Sigma(1:f,:) =  cov(X(idx==k)); 
+%      else
+%         Sigma(f*(k-1)+1:f*k,:) = cov(X(idx==k)); 
+%      end   
+% end
 
 disp ("******* INITIALIZATION DONE! *******")
 end
--- Lab2/Code/main.m
+++ Lab2/Code/main.m
@@ -6,22 +6,33 @@ profile on;
 T1 = load_untouch_nii('../Data/4/preprocessedT1.nii');
 T2 = load_untouch_nii('../Data/4/preprocessedFlair.nii');
 
+ground_truth = load_untouch_nii('../Data/5/LabelsForTesting.nii');
+
 x1 = [];
 x2 = [];
 
 x1_original = [];
 x2_original = [];
+GT = [];
 
 N = size(T1.img,1) * size(T1.img,2);
 for i = 1: size(T1.img,3)
     x1_original = vertcat(x1_original,reshape(double(T1.img(:,:,i)),N,1));
     x2_original = vertcat(x2_original,reshape(double(T2.img(:,:,i)),N,1));
-end
-x1 = x1_original;
-x2 = x2_original;
-x1(x1 <= 6) = [];
-x2(x2 <= 6) = [];
+    
+    GT = vertcat(GT,reshape(double(ground_truth.img(:,:,i)),N,1));
 
+end
+
+%Remove the backgroud pixels
+mask = GT > 0;
+mask2 = (GT == 0) * -50;
+x1 = (x1_original .* mask) + mask2;
+x2 = (x2_original .* mask) + mask2;
+x1(x1 == -50 ) = [];
+x2(x2 == -50 ) = [];
+
+sprintf('About to initialize ...');
 X = horzcat(x1, x2);
 
 
@@ -41,12 +52,12 @@ end
 logL = log_likelihood(X,alpha,Mu,Sigma);
 iteration = 0; 
 
-sprintf('Iteration ------- Log Likelihood diff.');
+sprintf('Iteration ------- Log Likelihood diff.')
 diff = 1000;
 
 N = size(X,1);
 f = size(X,2);
-while diff > 100
+while diff > 0.1
     iteration = iteration + 1;
     
     %[alpha_new, Mu_new, Sigma_new] = M_Step (X, W);
@@ -64,7 +75,7 @@ while diff > 100
     for k = 1:K
         Mu_new(k,:) = sum(W(:,k).* X)/Net(k);
     end
-
+    
     % UPDATE SIGMA
     Sigma_new = zeros(K*f, f);
     for k = 1:K
@@ -80,15 +91,17 @@ while diff > 100
             Sigma_new(f*(k-1)+1:f*k,:) = temp/Net(k);
          end
     end
+    %Sigma_new
     %% EXPECTATION %%
 
     W = zeros(N,K);
 
     for k =1:K
         if k == 1
-            W(:,k) = mvnpdf(X ,Mu(k,:), Sigma(1:f,:))*alpha(k);
+            W(:,k) = mvnpdf(X ,Mu_new(k,:), Sigma_new(1:f,:))*alpha_new(k);
         else
-            W(:,k)= mvnpdf(X, Mu(k,:), Sigma((f*(k-1))+1: f*k,:))*alpha(k);
+            
+            W(:,k)= mvnpdf(X, Mu_new(k,:), Sigma_new((f*(k-1))+1: f*k,:))*alpha_new(k);
         end
     end
 
@@ -101,9 +114,9 @@ while diff > 100
     temp = zeros(N,1);
     for k =1:K
         if k == 1
-            temp =  temp +mvnpdf(X ,Mu(k,:), Sigma(1:f,:))*alpha(k);
+            temp =  temp + mvnpdf(X ,Mu_new(k,:), Sigma_new(1:f,:))*alpha_new(k);
         else
-            temp = temp + mvnpdf(X, Mu(k,:), Sigma((f*(k-1))+1: f*k,:))*alpha(k);
+            temp = temp + mvnpdf(X, Mu_new(k,:), Sigma_new((f*(k-1))+1: f*k,:))*alpha_new(k);
         end
     end
 
@@ -111,7 +124,7 @@ while diff > 100
     
     diff = abs(logL-logL_new);
     logL = logL_new;
-    sprintf('%i    -------    %0.4f',iteration,diff);
+    sprintf('%i    -------    %0.4f',iteration,diff)
     
     
 end
@@ -119,19 +132,26 @@ end
 
 % Reconstruct segmented results
 [~, I]= (max(W'));
-final_seg = zeros(size(x1_original))
+final_seg = zeros(size(x1_original));
 N_orig =  size(x1_original,1);
 
+%Reintroduce the background
 index = 1;
 for i=1:N_orig
-    if x1_original(i) == 0
-        final_seg(i) = 0
+    if mask(i) == 0
+        final_seg(i) = 0;
     else
-        final_seg(i) = I(index)
-        index = index + 1
+        final_seg(i) = I(index);
+        index = index + 1;
     end
 end
-    
+
+%test the dice of a slice with ground truth
 final_seg = reshape(final_seg, size(T1.img));
-imshow(final_seg(:,:,25)',[]);
+
+seg = final_seg(:,:,25)';
+seg2 = seg; seg2(seg ==3) =2; seg2(seg == 2) = 3;
+truth = double(ground_truth.img(:,:,25))';
+figure;imshow(truth,[])
+figure;imshow(seg2,[])
 
